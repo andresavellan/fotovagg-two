f8eb76612896e248ca98c20c79c7564f073fe60d
Andrés Avellán <andresavellan@Andress-MacBook-Pro.local>
2021-08-13 16:46:29 +0200

    Correction of data being handled in server, that now is handled in the client side (params in fetch )

--- fotovagg-front/src/context/FotovaggContext.js
+++ fotovagg-front/src/context/FotovaggContext.js
@@ -1,124 +1,100 @@
-import React, { useEffect, useState, createContext, useRef } from 'react';
-import loader from './../assets/img/circles.svg'
-import axios from 'axios'
+import React, { useEffect, useState, createContext, useRef } from "react";
+import loader from "./../assets/img/circles.svg";
+import axios from "axios";
 
 export const FotovaggContext = createContext();
 
 const FotovaggContextProvider = (props) => {
-  //States 
+  //States
   const [error, setError] = useState(null);
   const [render, setRender] = useState(false);
   //Refs
   const isLoadedRef = useRef(false);
+  const pageRef = useRef(1);
+  const searchTermRef = useRef("Semla");
   const totalPagesRef = useRef();
   const newPhotosRef = useRef([]);
   const timerRef = useRef();
-  
+
   useEffect(() => {
-  //Fetch data. 1st round show without pause, second and forward with pause.
-  console.log('result')
-    fetch('/api')  
-    .then(res => res.json())
-      .then(
-        (res) => {
-          console.log(res[0].currentPage)
-          const result = JSON.parse(res[1])
-          // const currentPag = JSON.parse(res[0])
-          
+    //Fetch data. 1st round show without pause, second and forward with pause.
+    console.log("useEffect");
+    axios
+      .get("/api", {
+        params: {
+          pageNumber: pageRef.current,
+          searchTerm: searchTermRef.current,
+        },
+      })
+      .then(function (res) {
+          const result = JSON.parse(res.data)
           newPhotosRef.current = result.photos.photo;
           totalPagesRef.current = result.photos.pages;
-          isLoadedRef.current = true;
-          console.log(newPhotosRef.current)
-          // if (pageRef.current === 1) {
-          //   pageRef.current = 2;
-          //   setRender(render => !render);
-          //   //pauses from second round onwards
-          // } else if (pageRef.current > 1) {
-          //   timerRef.current = setTimeout(() => {
-          //     if (pageRef.current === totalPagesRef.current) {
-          //       pageRef.current = 1;
-          //     } else {
-          //       pageRef.current += 1;
-          //     }
-          //     setRender(render => !render);
-          //   }, 7000);
-          // }
 
-          if (res[0].currentPage === 1) {
-            //pageRef.current = 2;
+          isLoadedRef.current = true;
+          
+          if (pageRef.current === 1) {
+            pageRef.current = 2;
             setRender(render => !render);
             //pauses from second round onwards
-          } else if (res[0].currentPage > 1) {
+          } else if (pageRef.current > 1) {
             timerRef.current = setTimeout(() => {
-
-              // if (pageRef.current === totalPagesRef.current) {
-              //   pageRef.current = 1;
-              // } else {
-              //   pageRef.current += 1;
-              // }
-
-
+              if (pageRef.current === totalPagesRef.current) {
+                pageRef.current = 1;
+              } else {
+                pageRef.current += 1;
+              }
               setRender(render => !render);
             }, 7000);
           }
-
-
-
-          // setTimeout(()=>{
-          //   setRender(render => !render);
-          // }, 7000)
-        },
-        (error) => {
-          isLoadedRef.current = true;
-          setError(error);
-        }
-      )
-    
+      })
+      .catch(function (error) {
+        isLoadedRef.current = true;
+        setError(error);
+      })
+      .then(function () {
+        // always executed
+      });
+      
     return () => {
       clearTimeout(timerRef.current);
-    }
-      
+    };
   }, [render]);
-
 
   //search function from search component. Reset and ads new search term.
   const newSearch = (newSearchTerm) => {
-
     clearTimeout(timerRef.current);
     isLoadedRef.current = false;
-    setRender(render => !render);
-    
-    axios.post('/api', {
-      "filter": newSearchTerm
-      })
-    .then(res => {
-      console.log('this is the response', res)
-    })
-    .catch(error => {
-      console.log('heres is the errror', error)
-    })
-  }
+    pageRef.current = 1;
+    searchTermRef.current = newSearchTerm;
+    setRender((render) => !render);
+  };
 
   if (error) {
     return (
       <div className="error">
         <p>Error: {error.message}</p>
       </div>
-    )
+    );
   } else if (isLoadedRef.current !== true) {
-    return <div className="loader" ><img src={loader} width="160" alt="loader" /></div>;
+    return (
+      <div className="loader">
+        <img src={loader} width="160" alt="loader" />
+      </div>
+    );
   } else {
     return (
-      <FotovaggContext.Provider value={
-        {
+      <FotovaggContext.Provider
+        value={{
+          pageRef,
           newSearch,
           newPhotosRef,
-          render
-        }
-      }>
+          render,
+        }}
+      >
         {props.children}
       </FotovaggContext.Provider>
     );
   }
-}
-export default FotovaggContextProvider; 
+};
+export default FotovaggContextProvider;
--- server/index.js
+++ server/index.js
@@ -5,36 +5,16 @@ const request = require("postman-request")
 const PORT = process.env.PORT
 const API_KEY = process.env.API_KEY
 const app = express()
-const flickrBody = [
-    {
-        currentPage: 1,
-        searchTerm: 'Semla'
-    }
-]
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
-
-app.post("/api", function (req, res) {
-    flickrBody[0].currentPage = 1
-    flickrBody[0].searchTerm =  req.body.filter
-})
 
 app.get("/api", (req, res) => {
-   
-    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&content_type=1&media=photos&per_page=6&page=${flickrBody[0].currentPage}&text=${flickrBody[0].searchTerm}&format=json&nojsoncallback=1`;
-
+    let pageNumber = req.query.pageNumber
+    let searchTerm = req.query.searchTerm
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&content_type=1&media=photos&per_page=6&page=${pageNumber}&text=${searchTerm}&format=json&nojsoncallback=1`;
     request({ url: url }, (error, response) => {
         const data = response.body
-
-        flickrBody.splice(1,1,data)
-
-        res.json(flickrBody);
-
-        flickrBody[0].currentPage += 1
+        res.json(data);
     });
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
